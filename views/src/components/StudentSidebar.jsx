ad8d1b5b4b5960916b4a36011e43f7fd73a04e38
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-21 00:01:39 +0600

    StudentSidebar Component Created

---
+++ views/src/components/StudentSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StudentSidebar = () => {
+  return (
+    <aside>
+      <h1>StudentSidebar Component</h1>
+    </aside>
+  );
+};
+
+export default StudentSidebar;
