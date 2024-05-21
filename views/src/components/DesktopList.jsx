2e9baaf63c18f9049f82b93cdd37682590691a08
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-21 11:40:07 +0600

    Hero Section Created

--- views/src/components/DesktopList.jsx
+++ views/src/components/DesktopList.jsx
@@ -1,16 +1,32 @@
 import { NavLink } from 'react-router-dom';
 
 const DesktopList = () => {
+  const linkClasses = ({ isActive }) =>
+    isActive
+      ? 'text-sky-300 font-bold uppercase text-sm'
+      : 'text-slate-100 font-medium text-sm uppercase transition hover:text-sky-300';
+
   return (
-    <ul>
+    <ul className='flex items-center gap-5'>
       <li>
-        <NavLink to='/about'>About Us</NavLink>
+        <NavLink to='/' className={linkClasses}>
+          Home
+        </NavLink>
       </li>
       <li>
-        <NavLink to='/products'>Products</NavLink>
+        <NavLink to='/about' className={linkClasses}>
+          About Us
+        </NavLink>
       </li>
       <li>
-        <NavLink to='/contact'>Contact</NavLink>
+        <NavLink to='/products' className={linkClasses}>
+          Products
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to='/contact' className={linkClasses}>
+          Contact
+        </NavLink>
       </li>
     </ul>
   );
--- views/src/components/Navbar.jsx
+++ views/src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import BrandLogo from './BrandLogo';
+import DesktopList from './DesktopList';
 
 const Navbar = () => {
   return (
     <nav className='flex items-center justify-between'>
       <BrandLogo />
+
+      <div className='hidden lg:flex items-center'>
+        <DesktopList />
+      </div>
     </nav>
   );
 };
