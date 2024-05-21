7d2464aff66751f8d7526a57eef378223088ff11
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-21 23:39:19 +0600

    remix-icon imported

--- views/src/main.jsx
+++ views/src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
+// REMIX-ICONS
+import 'remixicon/fonts/remixicon.css';
+// TAILWIND-CSS
 import './assets/css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
