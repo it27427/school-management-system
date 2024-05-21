c009a5e36d799009da8517c171bb926e96a71402
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-21 12:14:59 +0600

    Admin Register Button Added

--- views/src/components/Hero.jsx
+++ views/src/components/Hero.jsx
@@ -3,15 +3,26 @@ import HeroThumb from '@/assets/images/hero/hero.png';
 const Hero = () => {
   return (
     <section className='flex flex-col items-center justify-center text-center min-h-screen'>
-      <div className='flex flex-col items-center justify-center text-center gap-4'>
-        <h1 className='text-slate-800 font-bold text-3xl'>
-          School Management System
-        </h1>
+      <div className='flex flex-col items-center justify-center text-center gap-5'>
+        <div className='flex flex-col items-center justify-center text-center gap-3'>
+          <h1 className='text-slate-800 font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl'>
+            Free Online School <br /> Management Software.
+          </h1>
+          <p className='text-sm text-black'>
+            Now you can manage your school, college, or any educational center{' '}
+            <br />
+            with eSkooly. It's 100% free for a lifetime with no limitations.
+          </p>
+        </div>
+
+        <div className='overflow-hidden'>
+          <button className='btn btn-neutral'>Admin Register</button>
+        </div>
       </div>
 
       <figure>
         <img
-          class='h-auto max-w-full object-cover'
+          className='h-auto max-w-full object-cover'
           src={HeroThumb}
           alt='HERO-IMAGE'
         />
