ec2ff298066ace777fb3569b974f91791cdb375a
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-21 23:38:37 +0600

    remixicons-installed

--- views/src/components/NotFound.jsx
+++ views/src/components/NotFound.jsx
@@ -1,10 +1,18 @@
-import EmptyLayout from 'layouts/EmptyLayout';
+import notfound from '@/assets/images/not-found/not-found.svg';
+import Button from './Button';
 
 const NotFound = () => {
   return (
-    <EmptyLayout>
-      <h1>Not Found</h1>
-    </EmptyLayout>
+    <div className='flex flex-col gap-6 items-center justify-center w-screen h-screen'>
+      <figure className='w-72 h-72 flex items-center justify-center'>
+        <img
+          src={notfound}
+          alt='NOT-FOUND-ICON'
+          className='h-auto max-w-full object-cover'
+        />
+      </figure>
+      <Button className='btn btn-primary'>Back to Home</Button>
+    </div>
   );
 };
 
--- views/src/pages/ErrorPage.jsx
+++ views/src/pages/ErrorPage.jsx
@@ -1,10 +1,11 @@
 import NotFound from '@/components/NotFound';
+import EmptyLayout from '@/layouts/EmptyLayout';
 
 const ErrorPage = () => {
   return (
-    <>
+    <EmptyLayout>
       <NotFound />
-    </>
+    </EmptyLayout>
   );
 };
 
