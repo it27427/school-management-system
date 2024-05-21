a3ab702cd0af8db639419b976568813d4abca40e
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-21 16:58:28 +0600

    ChooseUser Page Completed

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -3,12 +3,22 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import ChooseUser from '@/pages/Auth/ChooseUser';
 import HomePage from '@/pages/HomePage';
 
+/* AUTHENTICATION-PAGES */
+import AdminLogin from '@/pages/Auth/AdminLogin';
+import StudentLogin from '@/pages/Auth/StudentLogin';
+import TeacherLogin from '@/pages/Auth/TeacherLogin';
+
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/choose-user' element={<ChooseUser />} />
+
+        {/* AUTHENTICATION-PAGES */}
+        <Route path='/admin-login' element={<AdminLogin />} />
+        <Route path='/student-login' element={<StudentLogin />} />
+        <Route path='/teacher-login' element={<TeacherLogin />} />
       </Routes>
     </Router>
   );
--- views/src/pages/auth/ChooseUser.jsx
+++ views/src/pages/auth/ChooseUser.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import EmptyLayout from '@/layouts/EmptyLayout';
+import { Link } from 'react-router-dom';
 
 const ChooseUser = () => {
   return (
-    <div>
-      <h1>Choose User Page</h1>
-    </div>
+    <EmptyLayout>
+      <div className='grid place-items-center space-y-0 h-screen md:space-y-0 lg:grid-cols-3 lg:space-x-3 p-5'>
+        <div className='flex flex-col items-center justify-center text-center gap-3'>
+          <h2 className='font-bold uppercase text-black'>Admin</h2>
+          <Link to='/admin-login' className='btn btn-primary'>
+            Login as Admin
+          </Link>
+        </div>
+
+        <div className='flex flex-col items-center justify-center text-center gap-3'>
+          <h2 className='font-bold uppercase text-black'>Student</h2>
+          <Link to='/student-login' className='btn btn-primary'>
+            Login as Student
+          </Link>
+        </div>
+
+        <div className='flex flex-col items-center justify-center text-center gap-3'>
+          <h2 className='font-bold uppercase text-black'>Teacher</h2>
+          <Link to='/teacher-login' className='btn btn-primary'>
+            Login as Teacher
+          </Link>
+        </div>
+      </div>
+    </EmptyLayout>
   );
 };
 
