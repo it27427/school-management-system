f913104bf78cc7c3f7e000fba84737a81cbd7e73
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-21 10:54:34 +0600

    Header Added in RootLayout and Alias config

--- views/src/layouts/RootLayout.jsx
+++ views/src/layouts/RootLayout.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import Header from '@/components/Header';
 
 const RootLayout = ({ children }) => {
   return (
-    <main>
-      <div className='container'>{children}</div>
-    </main>
+    <>
+      <Header />
+      <main>
+        <div className='container'>{children}</div>
+      </main>
+    </>
   );
 };
 
--- views/vite.config.js
+++ views/vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      components: `${path.resolve(__dirname, './src/components/')}`,
+      public: `${path.resolve(__dirname, './public/')}`,
+      pages: path.resolve(__dirname, './src/pages'),
+      layouts: `${path.resolve(__dirname, './src/layouts')}`,
+      assets: `${path.resolve(__dirname, './src/assets')}`,
+    },
+  },
   server: {
     port: 3000,
   },
