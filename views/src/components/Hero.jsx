a8b3d5cceea4df3efac78bea47fa399f41bd3ff5
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-21 16:03:59 +0600

    Hero Section Completed

--- views/src/components/Hero.jsx
+++ views/src/components/Hero.jsx
@@ -3,14 +3,13 @@ import HeroThumb from '@/assets/images/hero/hero.png';
 const Hero = () => {
   return (
     <section className='flex flex-col items-center justify-center text-center min-h-screen'>
-      <div className='flex flex-col items-center justify-center text-center gap-5'>
-        <div className='flex flex-col items-center justify-center text-center gap-3'>
+      <div className='flex flex-col items-center justify-center text-center gap-5 lg:py-12'>
+        <div className='flex flex-col items-center justify-center text-center gap-4'>
           <h1 className='text-slate-800 font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl'>
             Free Online School <br /> Management Software.
           </h1>
-          <p className='text-sm text-black'>
-            Now you can manage your school, college, or any educational center{' '}
-            <br />
+          <p className='text-sm text-black lg:max-w-[460px]'>
+            Now you can manage your school, college, or any educational center
             with eSkooly. It's 100% free for a lifetime with no limitations.
           </p>
         </div>
