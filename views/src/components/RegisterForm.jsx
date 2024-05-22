8e50b8c7e573a6d298350ace2eb382d4b459fae5
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-22 13:21:34 +0600

    register form done

--- views/src/components/RegisterForm.jsx
+++ views/src/components/RegisterForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Button from './Button';
 import InputField from './InputField';
 
 const RegisterForm = () => {
@@ -12,8 +13,32 @@ const RegisterForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={handleSubmit}
+      className='flex flex-col gap-6 w-80 md:w-96 mx-auto'
+    >
       <div className='flex flex-col gap-5'>
+        <label className='input input-bordered flex items-center gap-2'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 16 16'
+            fill='currentColor'
+            className='w-4 h-4 opacity-70'
+          >
+            <path d='M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z' />
+          </svg>
+
+          <InputField
+            type='text'
+            id='regusername'
+            name='name'
+            className='grow'
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            placeholder='Enter User Name'
+          />
+        </label>
+
         <label className='input input-bordered flex items-center gap-2'>
           <svg
             xmlns='http://www.w3.org/2000/svg'
@@ -24,26 +49,37 @@ const RegisterForm = () => {
             <path d='M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z' />
             <path d='M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z' />
           </svg>
-          <input type='text' className='grow' placeholder='Email' />
+
+          <InputField
+            type='email'
+            id='regmail'
+            name='email'
+            className='grow'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='Enter Email Address'
+          />
         </label>
+
         <label className='input input-bordered flex items-center gap-2'>
           <svg
             xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 16 16'
+            viewBox='0 0 24 24'
+            width='16'
+            height='16'
             fill='currentColor'
-            className='w-4 h-4 opacity-70'
           >
-            <path d='M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z' />
+            <path d='M21 16.42V19.9561C21 20.4811 20.5941 20.9167 20.0705 20.9537C19.6331 20.9846 19.2763 21 19 21C10.1634 21 3 13.8366 3 5C3 4.72371 3.01545 4.36687 3.04635 3.9295C3.08337 3.40588 3.51894 3 4.04386 3H7.5801C7.83678 3 8.05176 3.19442 8.07753 3.4498C8.10067 3.67907 8.12218 3.86314 8.14207 4.00202C8.34435 5.41472 8.75753 6.75936 9.3487 8.00303C9.44359 8.20265 9.38171 8.44159 9.20185 8.57006L7.04355 10.1118C8.35752 13.1811 10.8189 15.6425 13.8882 16.9565L15.4271 14.8019C15.5572 14.6199 15.799 14.5573 16.001 14.6532C17.2446 15.2439 18.5891 15.6566 20.0016 15.8584C20.1396 15.8782 20.3225 15.8995 20.5502 15.9225C20.8056 15.9483 21 16.1633 21 16.42Z'></path>
           </svg>
-          <input type='text' className='grow' placeholder='Username' />
+
           <InputField
-            type='text'
-            id='userName'
-            name='userName'
+            type='tel'
+            id='regphone'
+            name='phone'
             className='grow'
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            placeholder='Enter UserName'
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            placeholder='Enter Phone Number'
           />
         </label>
 
@@ -60,17 +96,24 @@ const RegisterForm = () => {
               clipRule='evenodd'
             />
           </svg>
-          <input type='password' className='grow' value='password' />
           <InputField
-            type='text'
-            id='userName'
-            name='userName'
-            className=''
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
+            type='password'
+            id='regpassword'
+            name='password'
+            className='grow'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder='Enter Password'
           />
         </label>
       </div>
+
+      <Button
+        type='submit'
+        className='btn btn-primary text-md uppercase font-bold'
+      >
+        Register
+      </Button>
     </form>
   );
 };
