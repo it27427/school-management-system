1df20b815c68a5cb7c3ce18754b8ac2edc27a528
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-23 10:41:22 +0600

    RootLayout added in student-exam

--- views/src/pages/Students/Exam.jsx
+++ views/src/pages/Students/Exam.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import RootLayout from '@/layouts/RootLayout';
 
 const Exam = () => {
   return (
-    <div>
+    <RootLayout>
       <h1>Students Exam Page</h1>
-    </div>
+    </RootLayout>
   );
 };
 
