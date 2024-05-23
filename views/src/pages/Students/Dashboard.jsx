de9bdf28a3a0202940129aa05504665eab0e5150
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-23 10:40:53 +0600

    RootLayout added in student-dashboard

--- views/src/pages/Students/Dashboard.jsx
+++ views/src/pages/Students/Dashboard.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import RootLayout from '@/layouts/RootLayout';
 
 const Dashboard = () => {
   return (
-    <div>
+    <RootLayout>
       <h1>Students Dashboard Page</h1>
-    </div>
+    </RootLayout>
   );
 };
 
