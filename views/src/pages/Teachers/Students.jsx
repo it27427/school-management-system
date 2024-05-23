5a900759982e95f9661163433e49ddc35b305d0d
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-23 10:46:50 +0600

    RootLayout added in teacher-student page

--- views/src/pages/Teachers/Students.jsx
+++ views/src/pages/Teachers/Students.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import RootLayout from '@/layouts/RootLayout';
 
 const Students = () => {
   return (
-    <div>
+    <RootLayout>
       <h1>Teachers Students Page</h1>
-    </div>
+    </RootLayout>
   );
 };
 
