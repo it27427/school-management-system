cd3d3251d236e7946b78527329e26958b4028208
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-23 10:31:43 +0600

    RootLayout added in Admin-Announcement

--- views/src/pages/Admin/Announcement.jsx
+++ views/src/pages/Admin/Announcement.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import RootLayout from '@/layouts/RootLayout';
 
 const AdminAnnouncement = () => {
   return (
-    <div>
+    <RootLayout>
       <h1>Admin Announcement Page</h1>
-    </div>
+    </RootLayout>
   );
 };
 
