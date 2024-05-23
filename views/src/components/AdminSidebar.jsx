809b00f4e1d69ccd988335830cbfa5563c700585
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-23 12:58:40 +0600

    Admin Sidebar Completed

--- views/src/components/AdminSidebar.jsx
+++ views/src/components/AdminSidebar.jsx
@@ -1,18 +1,21 @@
 import Logo from '@/assets/images/logo/logo.webp';
 import { Link } from 'react-router-dom';
+import AdminList from './AdminList';
 
 const AdminSidebar = () => {
   return (
-    <aside className='sticky top-0 z-50 min-h-screen bg-sky-900'>
-      <div className='flex flex-col gap-6'>
-        <header>
-          <Link
-            to='/admin/dashboard'
-            className='flex items-center justify-center w-28 h-11'
-          >
-            <img src={Logo} alt='BRAND-LOGO' />
-          </Link>
-        </header>
+    <aside className='sticky top-0 z-50 min-h-screen bg-sky-950'>
+      <header className='w-full h-16 flex items-center justify-center sticky top-0 bg-sky-950 mb-2'>
+        <Link
+          to='/admin/dashboard'
+          className='flex items-center justify-center w-28 h-14'
+        >
+          <img src={Logo} alt='BRAND-LOGO' className='w-full h-full' />
+        </Link>
+      </header>
+
+      <div className='overflow-y-auto max-h-[calc(100% - 4rem)]'>
+        <AdminList />
       </div>
     </aside>
   );
