2839473d2b68ff7d9eb6e57d5f7087def448956e
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-23 14:19:26 +0600

    StudentList Updated and StudentSidebar Completed

--- views/src/components/StudentList.jsx
+++ views/src/components/StudentList.jsx
@@ -9,63 +9,38 @@ const StudentList = () => {
   return (
     <ul className='flex flex-col gap-0.5'>
       <li>
-        <NavLink to='/admin/dashboard' className={linkclassNames}>
+        <NavLink to='/student/dashboard' className={linkclassNames}>
           <i className='ri-dashboard-line'></i> Dashboard
         </NavLink>
       </li>
       <li>
-        <NavLink to='/admin/classes' className={linkclassNames}>
-          <i className='ri-school-line'></i> Classes
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to='/admin/students' className={linkclassNames}>
-          <i className='ri-team-line'></i> Students
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to='/admin/teachers' className={linkclassNames}>
-          <i className='ri-group-line'></i> Teachers
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to='/admin/assignment' className={linkclassNames}>
+        <NavLink to='/student/assignment' className={linkclassNames}>
           <i className='ri-attachment-line'></i> Assignments
         </NavLink>
       </li>
       <li>
-        <NavLink to='/admin/exam' className={linkclassNames}>
+        <NavLink to='/student/exam' className={linkclassNames}>
           <i className='ri-draft-line'></i> Exam
         </NavLink>
       </li>
       <li>
-        <NavLink to='/admin/performance' className={linkclassNames}>
+        <NavLink to='/student/performance' className={linkclassNames}>
           <i className='ri-line-chart-line'></i> Performance
         </NavLink>
       </li>
       <li>
-        <NavLink to='/admin/attendance' className={linkclassNames}>
+        <NavLink to='/student/attendance' className={linkclassNames}>
           <i className='ri-calendar-line'></i> Attendance
         </NavLink>
       </li>
       <li>
-        <NavLink to='/admin/library' className={linkclassNames}>
-          <i className='ri-building-4-line'></i> Library
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to='/admin/announcement' className={linkclassNames}>
+        <NavLink to='/student/announcement' className={linkclassNames}>
           <i className='ri-megaphone-line'></i> Announcement
         </NavLink>
       </li>
       <li>
-        <NavLink to='/admin/events' className={linkclassNames}>
-          <i className='ri-calendar-event-line'></i> Events and Calendar
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to='/admin/settings' className={linkclassNames}>
-          <i className='ri-settings-2-line'></i> Settings and Profile
+        <NavLink to='/student/profile' className={linkclassNames}>
+          <i className='ri-user-3-line'></i> Profile
         </NavLink>
       </li>
     </ul>
