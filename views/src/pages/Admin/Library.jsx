abfccef32254e40a46ad33296ca5e7d3143360fc
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-23 10:36:53 +0600

    RootLayout added in admin-library

--- views/src/pages/Admin/Library.jsx
+++ views/src/pages/Admin/Library.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import RootLayout from '@/layouts/RootLayout';
 
 const AdminLibrary = () => {
   return (
-    <div>
+    <RootLayout>
       <h1>Admin Library Page</h1>
-    </div>
+    </RootLayout>
   );
 };
 
