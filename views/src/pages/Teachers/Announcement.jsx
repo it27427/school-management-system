d070c34c67e096d11276aeccbe354411fb3e7f6f
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-23 10:42:50 +0600

    RootLayout added in teacher-announcement

--- views/src/pages/Teachers/Announcement.jsx
+++ views/src/pages/Teachers/Announcement.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import RootLayout from '@/layouts/RootLayout';
 
 const Announcement = () => {
   return (
-    <div>
+    <RootLayout>
       <h1>Teachers Announcement Teachers</h1>
-    </div>
+    </RootLayout>
   );
 };
 
