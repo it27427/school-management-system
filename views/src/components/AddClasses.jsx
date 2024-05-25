9f1f7bcf1182bea40eb8a5370682601675900fe8
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-25 12:41:03 +0600

    AddClasses Component Created and added to Admin Classes Page

---
+++ views/src/components/AddClasses.jsx
@@ -0,0 +1,30 @@
+import Button from '@/components/Button';
+import Form from '@/components/Form';
+import InputField from '@/components/InputField';
+
+const AddClasses = ({ clsName, setClsName, handleAddClasses }) => {
+  return (
+    <Form
+      onSubmit={handleAddClasses}
+      className='flex flex-col gap-4 max-w-xs md:max-w-md'
+    >
+      <InputField
+        type='text'
+        id='clsName'
+        name='clsName'
+        className='input input-bordered w-full'
+        onChange={(e) => setClsName(e.target.value)}
+        placeholder='Enter Class Name'
+        value={clsName}
+      />
+
+      <div className='flex justify-end'>
+        <Button type='submit' className='btn btn-primary'>
+          Add Class
+        </Button>
+      </div>
+    </Form>
+  );
+};
+
+export default AddClasses;
--- views/src/pages/Admin/Classes.jsx
+++ views/src/pages/Admin/Classes.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
+
 import AdminLayout from '@/layouts/AdminLayout';
 
+import AddClasses from '@/components/AddClasses';
+
 const AdminClasses = () => {
+  const [clsName, setClsName] = useState('');
+
+  const handleAddClasses = (e) => {
+    e.preventDefault();
+    console.log('Class added');
+  };
+
   return (
     <AdminLayout>
-      <h1>Admin Classes Page</h1>
+      <div className='flex flex-col gap-6'>
+        <h2 className='font-bold text-xl uppercase'>Add Classes</h2>
+
+        <AddClasses
+          clsName={clsName}
+          setClsName={setClsName}
+          handleAddClasses={handleAddClasses}
+        />
+      </div>
     </AdminLayout>
   );
 };
