8ea358ce7b1f1b36807b9f6700bb31e3abe809f8
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-25 23:15:04 +0600

    Card Height Added

--- views/src/components/Card.jsx
+++ views/src/components/Card.jsx
@@ -1,6 +1,6 @@
 const Card = ({ children }) => {
   return (
-    <div className='card bg-base-100 shadow-xl'>
+    <div className='card bg-base-100 shadow-xl h-full'>
       <div className='card-body'>{children}</div>
     </div>
   );
