f5fbec3e4474bd7336be9c2bee7d444265a28088
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-25 22:50:27 +0600

    Assignment Done

--- views/src/components/AddAssignment.jsx
+++ views/src/components/AddAssignment.jsx
@@ -1,4 +1,5 @@
 import Button from '@/components/Button';
+import Card from '@/components/Card';
 import Form from '@/components/Form';
 import InputField from '@/components/InputField';
 import TextArea from '@/components/TextArea';
@@ -15,55 +16,57 @@ const AddAssignment = ({
   handleAddAssignment,
 }) => {
   return (
-    <Form
-      onSubmit={handleAddAssignment}
-      className='flex flex-col gap-4 max-w-xs md:max-w-md'
-    >
-      <InputField
-        type='text'
-        id='assignmentTitle'
-        name='title'
-        className='input input-bordered w-full'
-        onChange={(e) => setAssignmentTitle(e.target.value)}
-        placeholder='Enter Assignment Title'
-        value={assignmentTitle}
-      />
+    <Card>
+      <Form
+        onSubmit={handleAddAssignment}
+        className='flex flex-col gap-4 max-w-xs md:max-w-md'
+      >
+        <InputField
+          type='text'
+          id='assignmentTitle'
+          name='title'
+          className='input input-bordered w-full'
+          onChange={(e) => setAssignmentTitle(e.target.value)}
+          placeholder='Enter Assignment Title'
+          value={assignmentTitle}
+        />
 
-      <TextArea
-        id='assignmentDescription'
-        name='description'
-        className='textarea input-textarea resize-none w-full'
-        onChange={(e) => setAssignmentDescription(e.target.value)}
-        placeholder='Enter Assignment Description'
-        value={assignmentDescription}
-      />
+        <TextArea
+          id='assignmentDescription'
+          name='description'
+          className='textarea textarea-bordered resize-none w-full'
+          onChange={(e) => setAssignmentDescription(e.target.value)}
+          placeholder='Enter Assignment Description'
+          value={assignmentDescription}
+        />
 
-      <InputField
-        type='text'
-        id='grade'
-        name='grade'
-        className='input input-bordered w-full'
-        onChange={(e) => setAssignmentGrade(e.target.value)}
-        placeholder='Enter Assignment Grade'
-        value={assignmentGrade}
-      />
+        <InputField
+          type='text'
+          id='grade'
+          name='grade'
+          className='input input-bordered w-full'
+          onChange={(e) => setAssignmentGrade(e.target.value)}
+          placeholder='Enter Assignment Grade'
+          value={assignmentGrade}
+        />
 
-      <InputField
-        type='text'
-        id='deadline'
-        name='deadline'
-        className='input input-bordered w-full'
-        onChange={(e) => setDeadline(e.target.value)}
-        placeholder='Enter Assignment Deadline'
-        value={deadline}
-      />
+        <InputField
+          type='text'
+          id='deadline'
+          name='deadline'
+          className='input input-bordered w-full'
+          onChange={(e) => setDeadline(e.target.value)}
+          placeholder='Enter Assignment Deadline'
+          value={deadline}
+        />
 
-      <div className='flex justify-end'>
-        <Button type='submit' className='btn btn-primary'>
-          Add Assignment
-        </Button>
-      </div>
-    </Form>
+        <div className='flex justify-end'>
+          <Button type='submit' className='btn btn-primary'>
+            Add Assignment
+          </Button>
+        </div>
+      </Form>
+    </Card>
   );
 };
 
--- views/src/components/AssignmentList.jsx
+++ views/src/components/AssignmentList.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import Card from '@/components/Card';
 
 const AssignmentList = () => {
   return (
     <div className='flex flex-col gap-6'>
       <h2 className='font-bold text-xl uppercase'>Assignment List</h2>
 
-      <ul>
-        <li>Assignment 1</li>
-      </ul>
+      <Card>
+        <ul>
+          <li>Assignment 1</li>
+        </ul>
+      </Card>
     </div>
   );
 };
