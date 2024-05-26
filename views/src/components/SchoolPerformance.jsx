dfb285eb9f1bc9b34815953ddde91899697a3015
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-26 10:31:05 +0600

    SchoolPerformance Component Created

---
+++ views/src/components/SchoolPerformance.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SchoolPerformance = ({}) => {
+  return <div className='flex flex-col'></div>;
+};
+
+export default SchoolPerformance;
--- views/src/pages/Admin/Performance.jsx
+++ views/src/pages/Admin/Performance.jsx
@@ -1,8 +1,43 @@
+import AdminLayout from '@/layouts/AdminLayout';
+
 const AdminPerformance = () => {
+  const schoolPerformanceData = {
+    averageScore: 85,
+    totalStudent: 200,
+  };
+
+  const individualPerformanceData = [
+    {
+      id: 1,
+      name: 'Istiak Hossain Tushar',
+      score: 90,
+    },
+    {
+      id: 2,
+      name: 'Rubaiyat Jahan Nishi',
+      score: 85,
+    },
+    {
+      id: 3,
+      name: 'Israt Jahan Hira',
+      score: 80,
+    },
+    {
+      id: 4,
+      name: 'Abdullah Al Mamun',
+      score: 75,
+    },
+    {
+      id: 5,
+      name: 'Mahadi Hasan',
+      score: 70,
+    },
+  ];
+
   return (
-    <>
+    <AdminLayout>
       <h1>Admin Performance Page</h1>
-    </>
+    </AdminLayout>
   );
 };
 
