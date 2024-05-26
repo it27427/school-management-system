ec9d051c97ddcb6ecd66f6a447f89f6b30e7af56
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-26 10:14:12 +0600

    Exam-details added in exam page

--- views/src/pages/Admin/Exam.jsx
+++ views/src/pages/Admin/Exam.jsx
@@ -1,9 +1,40 @@
+import { useState } from 'react';
+
+import AddExam from '@/components/AddExam';
 import AdminLayout from '@/layouts/AdminLayout';
 
 const AdminExam = () => {
+  const [name, setName] = useState('');
+  const [regNumber, setRegNumber] = useState('');
+  const [examClass, setExamClass] = useState('');
+  const [marks, setMarks] = useState('');
+
+  const handleAddExam = (e) => {
+    e.preventDefault();
+    console.log('Assignment Added');
+  };
+
   return (
     <AdminLayout>
-      <h1>Admin Exam Page</h1>
+      <div className='grid gap-6 lg:grid-cols-2'>
+        <div className='flex flex-col gap-6'>
+          <h2 className='font-bold text-xl uppercase'>Exam Details</h2>
+
+          <AddExam
+            name={name}
+            setName={setName}
+            regNumber={regNumber}
+            setRegNumber={setRegNumber}
+            examClass={examClass}
+            setExamClass={setExamClass}
+            marks={marks}
+            setMarks={setMarks}
+            handleAddExam={handleAddExam}
+          />
+        </div>
+
+        {/* <AssignmentList /> */}
+      </div>
     </AdminLayout>
   );
 };
