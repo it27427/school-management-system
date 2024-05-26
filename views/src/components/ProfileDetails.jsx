87b8d28aff5f80ace75481540b8dda4ad2a79bff
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-26 17:20:54 +0600

    ProfileDetails Completed

--- views/src/components/ProfileDetails.jsx
+++ views/src/components/ProfileDetails.jsx
@@ -1,5 +1,38 @@
-const ProfileDetails = () => {
-  return <div></div>;
+import Button from '@/components/Button';
+
+const ProfileDetails = ({ profileInfo }) => {
+  return (
+    <div className='flex flex-col gap-4'>
+      <div className='flex flex-col'>
+        <h2 className='text-lg font-bold'>Name</h2>
+        <p className='text-md font-medium'>{profileInfo.name}</p>
+      </div>
+
+      <div className='flex flex-col'>
+        <h2 className='text-lg font-bold'>Email Address</h2>
+        <p className='text-md font-medium'>{profileInfo.email}</p>
+      </div>
+
+      <div className='flex flex-col'>
+        <h2 className='text-lg font-bold'>Phone Number</h2>
+        <p className='text-md font-medium'>{profileInfo.phone}</p>
+      </div>
+
+      <div className='flex flex-col'>
+        <h2 className='text-lg font-bold'>Address</h2>
+        <p className='text-md font-medium'>{profileInfo.address}</p>
+      </div>
+
+      <div className='flex flex-col'>
+        <h2 className='text-lg font-bold'>Qualifications</h2>
+        <p className='text-md font-medium'>{profileInfo.qualification}</p>
+      </div>
+
+      <div className='mt-4'>
+        <Button className='btn btn-primary'>Edit Profile</Button>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileDetails;
--- views/src/pages/Admin/SettingsProfile.jsx
+++ views/src/pages/Admin/SettingsProfile.jsx
@@ -1,13 +1,26 @@
 import AdminLayout from '@/layouts/AdminLayout';
 
+import Card from '@/components/Card';
+import ProfileDetails from '@/components/ProfileDetails';
+
 const SettingsProfile = () => {
+  const profileInfo = {
+    name: 'Istiak Hossain Tushar',
+    email: 'istiak.tushar.it@gmail.com',
+    phone: '+8801615922742',
+    address: 'Grirdanarayonpur, Sherpur Sadar, Sherpur',
+    qualification: 'Bachelor of Education',
+  };
+
   return (
     <AdminLayout>
       <div className='grid gap-6 lg:grid-cols-2'>
         <div className='flex flex-col gap-6'>
           <h2 className='font-bold text-xl uppercase'>Profile Details</h2>
 
-          <ProfileDetails />
+          <Card>
+            <ProfileDetails profileInfo={profileInfo} />
+          </Card>
         </div>
       </div>
     </AdminLayout>
