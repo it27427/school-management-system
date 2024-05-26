fbcfd2af63aebb77b46191701649ba0e8b9ccb38
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-26 17:30:52 +0600

    EditProfile Component Completed

--- views/src/components/EditProfile.jsx
+++ views/src/components/EditProfile.jsx
@@ -1,10 +1,80 @@
-import React from 'react';
+import { useState } from 'react';
+import Button from './Button';
+import Form from './Form';
+import InputField from './InputField';
 
 const EditProfile = () => {
+  const [editName, setEditName] = useState('');
+  const [editEmail, setEditEmail] = useState('');
+  const [editPhone, setEditPhone] = useState('');
+  const [editAddress, setEditAddress] = useState('');
+  const [editQualification, setEditQualification] = useState('');
+
+  const handleEditProfile = (e) => {
+    e.preventDefault();
+    console.log('Profile Edited Done');
+  };
+
   return (
-    <div>
-      <h1>Edit Profile</h1>
-    </div>
+    <Form
+      onSubmit={handleEditProfile}
+      className='flex flex-col gap-4 max-w-xs md:max-w-md'
+    >
+      <InputField
+        type='text'
+        id='editname'
+        name='name'
+        className='input input-bordered w-full'
+        onChange={(e) => setEditName(e.target.value)}
+        placeholder='Enter Name'
+        value={editName}
+      />
+
+      <InputField
+        type='email'
+        id='editemail'
+        name='email'
+        className='input input-bordered w-full'
+        onChange={(e) => setEditEmail(e.target.value)}
+        placeholder='Enter Email'
+        value={editEmail}
+      />
+
+      <InputField
+        type='tel'
+        id='editephone'
+        name='phone'
+        className='input input-bordered w-full'
+        onChange={(e) => setEditPhone(e.target.value)}
+        placeholder='Enter Phone Number'
+        value={editPhone}
+      />
+
+      <TextArea
+        id='address'
+        name='address'
+        className='textarea textarea-bordered resize-none w-full'
+        onChange={(e) => setEditAddress(e.target.value)}
+        placeholder='Enter Address'
+        value={editAddress}
+      />
+
+      <InputField
+        type='text'
+        id='editequalification'
+        name='qualification'
+        className='input input-bordered w-full'
+        onChange={(e) => setEditQualification(e.target.value)}
+        placeholder='Enter Qualification'
+        value={editQualification}
+      />
+
+      <div className='flex justify-end'>
+        <Button type='submit' className='btn btn-primary'>
+          Edit Profile
+        </Button>
+      </div>
+    </Form>
   );
 };
 
