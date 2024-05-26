c642b899ccbb1dd063d10c4da175a132a8636716
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-26 17:11:10 +0600

    ProfileDetails Component Created

--- views/src/pages/Admin/SettingsProfile.jsx
+++ views/src/pages/Admin/SettingsProfile.jsx
@@ -3,7 +3,13 @@ import AdminLayout from '@/layouts/AdminLayout';
 const SettingsProfile = () => {
   return (
     <AdminLayout>
-      <h1>Admin Settings Profile Page</h1>
+      <div className='grid gap-6 lg:grid-cols-2'>
+        <div className='flex flex-col gap-6'>
+          <h2 className='font-bold text-xl uppercase'>Profile Details</h2>
+
+          <ProfileDetails />
+        </div>
+      </div>
     </AdminLayout>
   );
 };
