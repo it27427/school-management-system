13836e534196cf0dfdcac46883f66e4733c7aa28
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-27 09:58:51 +0600

    Student-Dashboard Overview

--- views/src/pages/Students/Dashboard.jsx
+++ views/src/pages/Students/Dashboard.jsx
@@ -1,9 +1,30 @@
 import StudentLayout from '@/layouts/StudentLayout';
 
+import CountCard from '@/components/CountCard';
+
 const Dashboard = () => {
   return (
     <StudentLayout>
-      <h1>Students Dashboard Page</h1>
+      <div className='flex flex-col gap-6'>
+        <h2 className='text-xl font-bold uppercase'>Overview</h2>
+
+        <div className='grid md:grid-cols-3 gap-4'>
+          <CountCard>
+            <h2 className='card-title'>Assignments</h2>
+            <p>5</p>
+          </CountCard>
+
+          <CountCard>
+            <h2 className='card-title'>Performances</h2>
+            <p>20</p>
+          </CountCard>
+
+          <CountCard>
+            <h2 className='card-title'>Terms</h2>
+            <p>1</p>
+          </CountCard>
+        </div>
+      </div>
     </StudentLayout>
   );
 };
