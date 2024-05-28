b7778cdf45d6ae6c5fc95dec7852e6e4f8b1ac9b
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-28 11:08:01 +0600

    teacher-dashboard

--- views/src/pages/Teachers/Dashboard.jsx
+++ views/src/pages/Teachers/Dashboard.jsx
@@ -1,9 +1,40 @@
 import TeacherLayout from '@/layouts/TeacherLayout';
 
+import CountCard from '@/components/CountCard';
+
 const Dashboard = () => {
   return (
     <TeacherLayout>
-      <h1>Teachers Dashboard</h1>
+      <div className='flex flex-col gap-6'>
+        <h2 className='text-xl font-bold uppercase'>Overview</h2>
+
+        <div className='grid md:grid-cols-3 gap-4'>
+          <CountCard>
+            <h2 className='card-title'>Students</h2>
+            <p>5</p>
+          </CountCard>
+
+          <CountCard>
+            <h2 className='card-title'>Classes</h2>
+            <p>5</p>
+          </CountCard>
+
+          <CountCard>
+            <h2 className='card-title'>Assignments</h2>
+            <p>5</p>
+          </CountCard>
+
+          <CountCard>
+            <h2 className='card-title'>Events</h2>
+            <p>20</p>
+          </CountCard>
+
+          <CountCard>
+            <h2 className='card-title'>Exam</h2>
+            <p>1</p>
+          </CountCard>
+        </div>
+      </div>
     </TeacherLayout>
   );
 };
