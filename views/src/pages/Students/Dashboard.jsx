f28a175abd6a8a3484225a41f4f54e6201893ced
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-28 14:19:14 +0600

    Student Dashboard

--- views/src/pages/Students/Dashboard.jsx
+++ views/src/pages/Students/Dashboard.jsx
@@ -5,24 +5,48 @@ import CountCard from '@/components/CountCard';
 const Dashboard = () => {
   return (
     <StudentLayout>
-      <div className='flex flex-col gap-6'>
-        <h2 className='text-xl font-bold uppercase'>Overview</h2>
+      <div className='flex flex-col gap-8'>
+        <div className='flex flex-col gap-6'>
+          <h2 className='text-xl font-bold uppercase'>Overview</h2>
 
-        <div className='grid md:grid-cols-3 gap-4'>
-          <CountCard>
-            <h2 className='card-title'>Assignments</h2>
-            <p>5</p>
-          </CountCard>
+          <div className='grid md:grid-cols-3 gap-4'>
+            <CountCard>
+              <h2 className='card-title'>Assignments</h2>
+              <p>3</p>
+            </CountCard>
 
-          <CountCard>
-            <h2 className='card-title'>Performances</h2>
-            <p>20</p>
-          </CountCard>
+            <CountCard>
+              <h2 className='card-title'>Performance</h2>
+              <p>2</p>
+            </CountCard>
 
-          <CountCard>
-            <h2 className='card-title'>Terms</h2>
-            <p>1</p>
-          </CountCard>
+            <CountCard>
+              <h2 className='card-title'>Term</h2>
+              <p>1</p>
+            </CountCard>
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-6'>
+          <h2 className='text-xl font-bold uppercase'>Recent Activity</h2>
+
+          <div className='grid md:grid-cols-3 gap-4'>
+            <CountCard>
+              <h2 className='card-title'>Students</h2>
+              <p>5</p>
+            </CountCard>
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-6'>
+          <h2 className='text-xl font-bold uppercase'>Upcoming Events</h2>
+
+          <div className='grid md:grid-cols-3 gap-4'>
+            <CountCard>
+              <h2 className='card-title'>Students</h2>
+              <p>5</p>
+            </CountCard>
+          </div>
         </div>
       </div>
     </StudentLayout>
--- views/src/pages/Teachers/Dashboard.jsx
+++ views/src/pages/Teachers/Dashboard.jsx
@@ -5,34 +5,36 @@ import CountCard from '@/components/CountCard';
 const Dashboard = () => {
   return (
     <TeacherLayout>
-      <div className='flex flex-col gap-6'>
-        <h2 className='text-xl font-bold uppercase'>Overview</h2>
+      <div className='flex flex-col gap-8'>
+        <div className='flex flex-col gap-6'>
+          <h2 className='text-xl font-bold uppercase'>Overview</h2>
 
-        <div className='grid md:grid-cols-3 gap-4'>
-          <CountCard>
-            <h2 className='card-title'>Students</h2>
-            <p>5</p>
-          </CountCard>
+          <div className='grid md:grid-cols-3 gap-4'>
+            <CountCard>
+              <h2 className='card-title'>Students</h2>
+              <p>5</p>
+            </CountCard>
 
-          <CountCard>
-            <h2 className='card-title'>Classes</h2>
-            <p>5</p>
-          </CountCard>
+            <CountCard>
+              <h2 className='card-title'>Classes</h2>
+              <p>5</p>
+            </CountCard>
 
-          <CountCard>
-            <h2 className='card-title'>Assignments</h2>
-            <p>5</p>
-          </CountCard>
+            <CountCard>
+              <h2 className='card-title'>Assignments</h2>
+              <p>5</p>
+            </CountCard>
 
-          <CountCard>
-            <h2 className='card-title'>Events</h2>
-            <p>20</p>
-          </CountCard>
+            <CountCard>
+              <h2 className='card-title'>Events</h2>
+              <p>20</p>
+            </CountCard>
 
-          <CountCard>
-            <h2 className='card-title'>Exam</h2>
-            <p>1</p>
-          </CountCard>
+            <CountCard>
+              <h2 className='card-title'>Exam</h2>
+              <p>1</p>
+            </CountCard>
+          </div>
         </div>
       </div>
     </TeacherLayout>
