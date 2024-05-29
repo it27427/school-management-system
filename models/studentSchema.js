4e8110f029abb2ca83321ff0458ae1ab135d174a
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-29 22:21:43 +0600

    student model created

---
+++ models/studentSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: Sting,
+    required: true,
+    trim: true,
+  },
+  registrationNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  grade: {
+    type: String,
+    required: true,
+  },
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+module.exports = Student;
