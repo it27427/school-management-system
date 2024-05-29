bbd98d8e3fec2850922ad8adef637759f24457e7
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-29 13:08:19 +0600

    Teacher Assignment Done

--- views/src/pages/Teachers/Assignment.jsx
+++ views/src/pages/Teachers/Assignment.jsx
@@ -1,9 +1,40 @@
 import TeacherLayout from '@/layouts/TeacherLayout';
+import { useState } from 'react';
+
+import AddAssignment from '@/components/AddAssignment';
+import AssignmentList from '@/components/AssignmentList';
 
 const Assignment = () => {
+  const [assignmentTitle, setAssignmentTitle] = useState('');
+  const [assignmentDescription, setAssignmentDescription] = useState('');
+  const [assignmentGrade, setAssignmentGrade] = useState('');
+  const [deadline, setDeadline] = useState('');
+
+  const handleAddAssignment = (e) => {
+    e.preventDefault();
+    console.log('Assignment Added');
+  };
   return (
     <TeacherLayout>
-      <h1>Teachers Assignment Page</h1>
+      <div className='grid gap-6 lg:grid-cols-2'>
+        <div className='flex flex-col gap-6'>
+          <h2 className='font-bold text-xl uppercase'>Add Assignment</h2>
+
+          <AddAssignment
+            assignmentTitle={assignmentTitle}
+            setAssignmentTitle={setAssignmentTitle}
+            assignmentDescription={assignmentDescription}
+            setAssignmentDescription={setAssignmentDescription}
+            assignmentGrade={assignmentGrade}
+            setAssignmentGrade={setAssignmentGrade}
+            deadline={deadline}
+            setDeadline={setDeadline}
+            handleAddAssignment={handleAddAssignment}
+          />
+        </div>
+
+        <AssignmentList />
+      </div>
     </TeacherLayout>
   );
 };
