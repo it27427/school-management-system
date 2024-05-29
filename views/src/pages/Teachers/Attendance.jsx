c7ecbfc546a7874f3f76991cf274fab681db0f31
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-29 13:15:06 +0600

    teacher-attendance done

--- views/src/pages/Teachers/Attendance.jsx
+++ views/src/pages/Teachers/Attendance.jsx
@@ -1,9 +1,18 @@
+import AddAttendance from '@/components/AddAttendance';
+import Card from '@/components/Card';
 import TeacherLayout from '@/layouts/TeacherLayout';
 
 const Attendance = () => {
   return (
     <TeacherLayout>
-      <h1>Teachers Attendance Page</h1>
+      <div className='grid gap-6 lg:grid-cols-2'>
+        <div className='flex flex-col gap-6'>
+          <h2 className='font-bold text-xl uppercase'>Attendance</h2>
+          <Card>
+            <AddAttendance />
+          </Card>
+        </div>
+      </div>
     </TeacherLayout>
   );
 };
