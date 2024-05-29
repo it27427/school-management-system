4bdf7aae92381c29ae343ce37a99c919d9dda4cb
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-29 13:19:12 +0600

    Teacher Profile-Details done

--- views/src/pages/Teachers/Profile.jsx
+++ views/src/pages/Teachers/Profile.jsx
@@ -1,9 +1,28 @@
 import TeacherLayout from '@/layouts/TeacherLayout';
 
+import Card from '@/components/Card';
+import ProfileDetails from '@/components/ProfileDetails';
+
 const Profile = () => {
+  const profileInfo = {
+    name: 'Istiak Hossain Tushar',
+    email: 'istiak.tushar.it@gmail.com',
+    phone: '+8801615922742',
+    address: 'Grirdanarayonpur, Sherpur Sadar, Sherpur',
+    qualification: 'Bachelor of Education',
+  };
+
   return (
     <TeacherLayout>
-      <h1>Teachers Profile Page</h1>
+      <div className='grid gap-6 lg:grid-cols-2'>
+        <div className='flex flex-col gap-6'>
+          <h2 className='font-bold text-xl uppercase'>Profile Details</h2>
+
+          <Card>
+            <ProfileDetails profileInfo={profileInfo} />
+          </Card>
+        </div>
+      </div>
     </TeacherLayout>
   );
 };
