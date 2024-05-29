a735e2f89a89f7204653271f54f2aac67c79cd20
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-29 13:16:46 +0600

    teacher-announcements

--- views/src/pages/Teachers/Announcement.jsx
+++ views/src/pages/Teachers/Announcement.jsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
+
 import TeacherLayout from '@/layouts/TeacherLayout';
 
+import AddAnnouncement from '@/components/AddAnnouncement';
+import AnnouncementList from '@/components/AnnouncementList';
+
 const Announcement = () => {
+  const [announcement, setAnnouncement] = useState('');
+
+  const handleAddAnnouncement = (e) => {
+    e.preventDefault();
+    console.log('Announcement Added', announcement);
+  };
+
   return (
     <TeacherLayout>
-      <h1>Teachers Announcement Teachers</h1>
+      <div className='grid gap-6 lg:grid-cols-2'>
+        <div className='flex flex-col gap-6'>
+          <h2 className='font-bold text-xl uppercase'>Add Announcement</h2>
+
+          <AddAnnouncement
+            announcement={announcement}
+            setAnnouncement={setAnnouncement}
+            handleAddAnnouncement={handleAddAnnouncement}
+          />
+        </div>
+
+        <AnnouncementList />
+      </div>
     </TeacherLayout>
   );
 };
