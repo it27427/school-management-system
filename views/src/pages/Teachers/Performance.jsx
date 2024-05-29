05bd7f7dff3f7771b6ea43938ced2e03e1be3f04
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-29 13:14:07 +0600

    Teacher-Performance Done

--- views/src/pages/Teachers/Performance.jsx
+++ views/src/pages/Teachers/Performance.jsx
@@ -1,9 +1,64 @@
 import TeacherLayout from '@/layouts/TeacherLayout';
 
+import Card from '@/components/Card';
+import IndividualPerformance from '@/components/IndividualPerformance';
+import SchoolPerformance from '@/components/SchoolPerformance';
+
 const Performance = () => {
+  const schoolPerformanceData = {
+    averageScore: 85,
+    totalStudent: 200,
+  };
+
+  const individualPerformanceData = [
+    {
+      id: 1,
+      name: 'Istiak Hossain Tushar',
+      score: 90,
+    },
+    {
+      id: 2,
+      name: 'Rubaiyat Jahan Nishi',
+      score: 85,
+    },
+    {
+      id: 3,
+      name: 'Israt Jahan Hira',
+      score: 80,
+    },
+    {
+      id: 4,
+      name: 'Abdullah Al Mamun',
+      score: 75,
+    },
+    {
+      id: 5,
+      name: 'Mahadi Hasan',
+      score: 70,
+    },
+  ];
+
   return (
     <TeacherLayout>
-      <h1>Teachers Performance Page</h1>
+      <div className='flex flex-col gap-8'>
+        <div className='flex flex-col gap-3'>
+          <h2 className='text-xl font-bold uppercase'>School Performance</h2>
+          <Card>
+            <SchoolPerformance schoolPerformanceData={schoolPerformanceData} />
+          </Card>
+        </div>
+
+        <div className='flex flex-col gap-3'>
+          <h2 className='text-xl font-bold uppercase'>
+            Individual Performance
+          </h2>
+          <Card>
+            {individualPerformanceData.map((student) => (
+              <IndividualPerformance student={student} key={student.id} />
+            ))}
+          </Card>
+        </div>
+      </div>
     </TeacherLayout>
   );
 };
