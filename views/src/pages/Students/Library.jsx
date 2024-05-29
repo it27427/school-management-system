684da92c0f04e263bc6a2cbc05d0c36c08828e19
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-29 12:30:22 +0600

    StudentLibrary done

--- views/src/pages/Students/Library.jsx
+++ views/src/pages/Students/Library.jsx
@@ -1,11 +1,13 @@
 import StudentLayout from '@/layouts/StudentLayout';
 
 const StudentLibrary = () => {
-  <StudentLayout>
-    <div className='flex flex-col gap-6'>
-      <h2 className='text-xl font-bold uppercase'>Library</h2>
-    </div>
-  </StudentLayout>;
+  return (
+    <StudentLayout>
+      <div className='flex flex-col gap-6'>
+        <h2 className='text-xl font-bold uppercase'>Library</h2>
+      </div>
+    </StudentLayout>
+  );
 };
 
 export default StudentLibrary;
