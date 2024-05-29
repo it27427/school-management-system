2d80e6cae074d935d670b0ce4bf1fed9f507f12f
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-29 12:01:42 +0600

    Performance page done

--- views/src/pages/Students/Performance.jsx
+++ views/src/pages/Students/Performance.jsx
@@ -1,9 +1,59 @@
+import { useRef } from 'react';
+import { Line } from 'react-chartjs-2';
+
+import Card from '@/components/Card';
+
 import StudentLayout from '@/layouts/StudentLayout';
 
 const Performance = () => {
+  const chartRef = useRef();
+
+  const performanceData = {
+    months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+    marks: [80, 75, 90, 85, 70, 95],
+    totalMarks: 520,
+  };
+
+  const lineChartData = {
+    label: performanceData.months,
+    datasets: [
+      {
+        label: 'Performance Trends',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: '#007bff',
+        borderColor: '#007bff',
+        data: performanceData.marks,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
   return (
     <StudentLayout>
-      <h1>Students Performance Page</h1>
+      <div className='flex flex-col gap-6'>
+        <h2 className='text-xl font-bold uppercase'>Performances</h2>
+
+        <div className='grid md:grid-cols-3 gap-4'>
+          <div className='flex flex-col items-center justify-center text-center gap-4'>
+            <Card>
+              <Line ref={chartRef} data={lineChartData} options={options} />
+            </Card>
+
+            <h2 className='text-lg font-bold'>
+              Total Marks: {performanceData.totalMarks}
+            </h2>
+          </div>
+        </div>
+      </div>
     </StudentLayout>
   );
 };
