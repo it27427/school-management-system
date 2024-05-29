23ed9905ed27f2e2b7a960941413a465eff8d6d7
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-29 22:57:15 +0600

    error.handler function created

--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -1,5 +1,16 @@
-const handleValidationError = () => {};
+const handleValidationError = (message, statusCode) => {
+  const error = new Error(message);
+  error.statusCode = statusCode;
+  throw error;
+};
+
+const errorHandler = (error, req, res, next) => {
+  const statusCode = error.statusCode || 500;
+  const message = error.message || 'Internal Server Error';
+  res.status(statusCode).json({ success: false, message });
+};
 
 module.exports = {
   handleValidationError,
+  errorHandler,
 };
