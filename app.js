9af7f195535c9f883ea707d91df06033cf3a3db3
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-30 11:29:04 +0600

    express.urlencoded added in app

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('dev'));
 
