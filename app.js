d2ffd6d3c46651df0a1832331e1a2a5bcb2cea9a
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-31 00:06:30 +0600

    assignment model controller and router file created

--- app.js
+++ app.js
@@ -22,11 +22,13 @@ const studentRouter = require('./routes/student.router');
 const eventRouter = require('./routes/event.router');
 const libraryRouter = require('./routes/library.router');
 const announcementRouter = require('./routes/announcement.router');
+const assignmentRouter = require('./routes/assignment.router');
 
 app.use('/api/v1/students', studentRouter);
 app.use('/api/v1/events', eventRouter);
 app.use('/api/v1/library', libraryRouter);
 app.use('/api/v1/announcements', announcementRouter);
+app.use('/api/v1/assignments', assignmentRouter);
 
 app.use((error, req, res, next) => {
   errorHandler(error, req, res, next);
