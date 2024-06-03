140d13745ddcbee4fd1b430e03a1b4022bc8e660
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-03 08:43:45 +0600

    Assignment List done

--- views/src/components/AssignmentList.jsx
+++ views/src/components/AssignmentList.jsx
@@ -1,14 +1,33 @@
 import Card from '@/components/Card';
 
-const AssignmentList = () => {
+const AssignmentList = ({ assignments }) => {
   return (
     <div className='flex flex-col gap-6'>
       <h2 className='font-bold text-xl uppercase'>Assignment List</h2>
 
       <Card>
-        <ul>
-          <li>Assignment 1</li>
-        </ul>
+        <div className='lg:max-h-64 overflow-hidden lg:overflow-y-scroll'>
+          <table className='table'>
+            <thead className='sticky top-0 w-full bg-white'>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Subject</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {assignments.map((assignment, index) => (
+                <tr key={index}>
+                  <td>{assignment.title}</td>
+                  <td>{assignment.description}</td>
+                  <td>{assignment.grade}</td>
+                  <td>{assignment.deadline}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </Card>
     </div>
   );
